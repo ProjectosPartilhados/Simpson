d082846ab750aeafb25e6e40a86f0464a74f9304
Miguel Vasco Macamo <miguelvascomacamo@gmail.com>
2017-11-10 17:05:20 -0800

    ficheiros basicos do java

--- src/simpson/Simpson.java
+++ src/simpson/Simpson.java
@@ -20,8 +20,15 @@ public class Simpson {
         int n = 8;
         double h;
         double pa = limite_inferior;
+        double x=1;
+        double f1=0 ,fn=0, fi;
 
         h = (limite_superior - limite_inferior) / n;
+        
+         // 4 impares e 2 pares
+         
+        
+         double formula =h/3*(f1+fn);
 
         System.out.println("h= " + h);
         for (int i = 0; i <= n; i++) {
@@ -34,12 +41,20 @@ public class Simpson {
            // System.out.println(i);
             if ((i != 0) && (i !=n) && ((i % 2) == 0)) {
              //   System.out.println("I  Par = " + i);
+            }else{
+                if ((i != 0) &&(i !=n)) {
+                    //System.out.println("I  Impar = " + i);
+                }
+               
             }
-            if ((i != 0) && (i !=n) && ((i % 3) == 0)) {
-                  System.out.println("I  Impar = " + i);
-            } 
-
+           
             //System.out.println(pa);
+             // double funcao = 1/x; 
+            double x2=0 ;
+           // x2=limite_inferior;
+            
+           x2= x2+ limite_inferior + h;
+            System.out.println("X= "+x2);
 
         }
 
