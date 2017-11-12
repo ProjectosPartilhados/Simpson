d48b517839f9f78bc4e95f7f1d8eb9e62f123e8b
Miguel Vasco Macamo <miguelvascomacamo@gmail.com>
2017-11-11 23:14:50 -0800

    fazendo as somas de valores pares, impares e extremos

--- src/simpson/Simpson.java
+++ src/simpson/Simpson.java
@@ -28,12 +28,8 @@ public class Simpson {
         h = (limite_superior - limite_inferior) / n;
 
         // 4 impares e 2 pares
-      
-       double soma_par=0, soma_impar, soma_extremos=0;
-        
-        
-          
-        
+        double soma_par = 0, soma_impar = 0, soma_extremos = 0;
+
         System.out.println("h= " + h);
         for (int i = 0; i <= n; i++) {
             if (i == 0) {
@@ -46,25 +42,34 @@ public class Simpson {
             if ((i != 0) && (i != n) && ((i % 2) == 0)) {
                 System.out.println("I  Par = " + i);
                 funcao_par = pa * pa;
+                soma_par = soma_par + funcao_par;
                 System.out.println("Valor de Cada Funcao Par:" + funcao_par);
-                 System.out.println("F"+i+" = "+funcao_par);
+                System.out.println("F" + i + " = " + funcao_par);
+                System.out.println("Soma De Valores Pares" + soma_par);
             } else {
                 // Quando FOR IMPAR
                 if ((i != 0) && (i != n)) {
                     System.out.println("I  Impar = " + i);
                     funcao_impar = pa * pa;
-                        System.out.println("Valor de Cada Funcao ImPar:" + funcao_impar);
-                        System.out.println("F"+i+" = "+funcao_impar);
+                    soma_impar = soma_impar + funcao_impar;
 
-                }else{
-                 double fo= pa*pa;
-                    System.out.println("indice"+i);
-                    System.out.println("F"+i+" = "+fo);
+                    System.out.println("Valor de Cada Funcao ImPar:" + funcao_impar);
+                    System.out.println("F" + i + " = " + funcao_impar);
+                    System.out.println("Soma De Valores Impares" + soma_impar);
+
+                } else {
+                    double fo = pa * pa;
+                    soma_extremos = soma_extremos + fo;
+
+                    System.out.println("indice" + i);
+                    System.out.println("F" + i + " = " + fo);
+                    
+                    System.out.println("Soma De Extremos" + soma_par);
                 }
 
             }
 
-           double formula = h / 3 * (f1 + fn);
+            double formula = h / 3 * (f1 + fn);
         }
 
     }
